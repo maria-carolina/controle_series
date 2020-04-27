d02d6e15e3207f5593b59757316044bb2ff18085
maria-carolina <mariacarolina1701@outlook.com>
2020-04-27 14:27:43 -0300

    Update migrations

--- database/migrations/2020_04_27_162024_create_temporadas_table.php
+++ database/migrations/2020_04_27_162024_create_temporadas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CriarTabelaSerie extends Migration
+class CreateTemporadasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,13 @@ class CriarTabelaSerie extends Migration
      */
     public function up()
     {
-        Schema::create('series', function(Blueprint $table) {
+        Schema::create('temporadas', function (Blueprint $table) {
             $table->integer('id')->autoIncrement();
-            $table->string('nome');
+            $table->integer('numero');
+            $table->integer('serie_id');
+//            $table->foreign('serie_id')
+//                ->references('id')
+//                ->on('series');
         });
     }
 
@@ -26,6 +30,6 @@ class CriarTabelaSerie extends Migration
      */
     public function down()
     {
-        Schema::drop('series');
+        Schema::dropIfExists('temporadas');
     }
 }
--- database/migrations/2020_04_27_162126_create_episodios_table.php
+++ database/migrations/2020_04_27_162126_create_episodios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CriarTabelaSerie extends Migration
+class CreateEpisodiosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class CriarTabelaSerie extends Migration
      */
     public function up()
     {
-        Schema::create('series', function(Blueprint $table) {
+        Schema::create('episodios', function (Blueprint $table) {
             $table->integer('id')->autoIncrement();
-            $table->string('nome');
+            $table->integer('numero');
+            $table->integer('temporada_id');
+            $table->foreign('temporada_id')->references('id')->on('temporadas');
         });
     }
 
@@ -26,6 +28,6 @@ class CriarTabelaSerie extends Migration
      */
     public function down()
     {
-        Schema::drop('series');
+        Schema::dropIfExists('episodios');
     }
 }
